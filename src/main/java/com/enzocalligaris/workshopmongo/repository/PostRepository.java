c7a73316e555a7e8b950d0c2e420e8d63047f3f3
EnzoCalligaris <enzocontatobr@gmail.com>
2025-08-21 12:01:53 -0300

    Consulta simples com @Query

--- src/main/java/com/enzocalligaris/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/enzocalligaris/workshopmongo/repository/PostRepository.java
@@ -1,14 +1,17 @@
 package com.enzocalligaris.workshopmongo.repository;
 
 import com.enzocalligaris.workshopmongo.domain.Post;
-import com.enzocalligaris.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
